d249921d25a133246057053d303f1993057656a1
7cmdehdrb <7cmdehdrb@naver.com>
2020-07-20 16:11:34 +0900

    First

---
+++ server/models/users.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const userSchema = new mongoose.Schema({
+    email: { type: String, unique: true, required: true },
+    password: { type: String, unique: false, required: true },
+    nickname: { type: String, unique: true, required: true },
+    guild: { type: String, unique: false, required: false },
+    farm: { type: String, unique: false, required: false },
+});
+
+export const userModel = mongoose.model("User", userSchema);
